1e94101789a36e2a338ea1802bcc049fb61afcd4
Stefan Streichsbier <stefan@streichsbier.at>
2023-07-13 19:29:41 +0700

    Update vulns.exs

--- vulns.exs
+++ vulns.exs
@@ -5,4 +5,10 @@ defmodule PasswordCompare do
       false -> :entry_denied_op1
     end
   end
+  def option_two(password, bcrypt_salted_hash) do
+    case Bcrypt.verify_pass(password, bcrypt_salted_hash) do
+      true -> :entry_granted_op2
+      false -> :entry_denied_op2
+    end
+  end
 end
